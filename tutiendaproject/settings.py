cda2bf9200685a8bff5c8c0a9c663ff1c6fad04a
LizethBustos <lkbe97@gmail.com>
2022-09-28 18:59:28 -0500

    add django-cors

--- tutiendaproject/settings.py
+++ tutiendaproject/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tutiendaapp'
+    'tutiendaapp',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -48,7 +50,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "https://tutienda-fe.heroku.com/"
+]
+
+
+#faltan rest framework
+
 
 AUTH_USER_MODEL = 'tutiendaapp.User'
 
