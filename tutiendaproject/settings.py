a5ceb34b82a626b54d2425efed41feb24fdf8c5e
Luis Miguel Mesa <luismg1032@gmail.com>
2022-09-30 13:49:40 -0500

    quitando el allow

--- tutiendaproject/settings.py
+++ tutiendaproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kj5iu*vkki!w(9fz0m+oo!y^%3y)q-du@)ar9a(=myya$49vi&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["tutienda-be.herokuapp.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
